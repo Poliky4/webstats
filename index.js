a1dcd340cf6640dd7a96a0cc17a47823f19b19c8
Poliky4 <pontusmolin@me.com>
2017-08-04 14:17:21 +0200

    prod/dev mode with npm run dev/prod, serving static for ssl

--- index.js
+++ index.js
@@ -2,19 +2,49 @@ const express = require('express');
 
 const app = express();
 
-// Returns requested webstats
-app.get('/webstats', (req, res) => {
-    res.send('Eyo!');
-});
+const prod = process.argv[2] === 'prod';
+const PORT = prod ? '443' : '1337'
+
+const utils = new Utils();
+
+if(prod) app.use(express.static('static'));
+
+// Returns requested webstats or all?
+// app.get('/webstats', (req, res) => {
+//     res.send('Eyo!');
+// });
 
 // Stores received webstats
 app.post('/webstats', (req, res) => {
-    console.log('POST!')
-    console.log(req)
-    console.log(res)
-    res.send('Eyo!');
+
+    utils.getHttpData(req).then(data => {
+        
+        console.log(data) // replace with db stuff ^^
+
+    });
 });
 
-app.listen(1337, _ => {
-    console.log('Webstats starting on port 1337 at', new Date().toUTCString());
+app.listen(1337, _ => { // replace with PORT
+    console.log('Webstats starting at port 1337 on', new Date().toUTCString());
 });
+
+function Utils(){
+    
+    function getHttpData(req){
+        return new Promise((resolve, reject) => {
+            let dataStr = '';
+            req.on('data', data => {
+                dataStr += data;
+            });
+            req.on('end', () => {
+                resolve(JSON.parse(dataStr));
+            });            
+        });
+    }
+
+    return {
+
+        getHttpData: getHttpData,
+
+    }
+}
